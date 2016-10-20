143768cfdd4bcf3792af9d25850adbbaaadd10df
HemalPatil <hempat02@gmail.com>
2016-10-20 14:41:11 +0530

    collapsing foreground added to aboutevent

--- src/main/java/com/hemal/eventhub2/AboutClubActivity.java
+++ src/main/java/com/hemal/eventhub2/AboutClubActivity.java
@@ -3,6 +3,7 @@ package com.hemal.eventhub2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class AboutClubActivity extends AppCompatActivity
 {
@@ -18,5 +19,15 @@ public class AboutClubActivity extends AppCompatActivity
 		setSupportActionBar(toolbar);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setDisplayShowHomeEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if(item.getItemId() == android.R.id.home)
+		{
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
